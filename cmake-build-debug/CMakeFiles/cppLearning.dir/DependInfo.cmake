fbe75fe6cae25709e01ae61bd905c6115943ce32
Desktop <1105770801@qq.com>
2023-09-12 23:11:13 +0800

    upgrade count_imgs_and_videos

---
+++ cmake-build-debug/CMakeFiles/cppLearning.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/MyCodeProject/CLionProjects/CppTools/main.cpp" "CMakeFiles/cppLearning.dir/main.cpp.obj" "gcc" "CMakeFiles/cppLearning.dir/main.cpp.obj.d"
+  "E:/MyCodeProject/CLionProjects/CppTools/mytools.cpp" "CMakeFiles/cppLearning.dir/mytools.cpp.obj" "gcc" "CMakeFiles/cppLearning.dir/mytools.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
