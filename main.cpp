669e0f8ca4edf2e94c1384784f10a0d9f53a50f7
Desktop <1105770801@qq.com>
2023-09-13 00:12:01 +0800

    update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 
+int64_t GetFolderSize(const fs::path& folderPath) {
+    int64_t size = 0;
+
+    for (const auto& entry : fs::recursive_directory_iterator(folderPath)) {
+        if (fs::is_regular_file(entry)) {
+            size += fs::file_size(entry);
+        }
+    }
+
+    return size;
+}
 
 int main(){
 
@@ -12,8 +23,8 @@ int main(){
 //    tools.delete_files("E:\\BaiduNetdiskDownload\\c++", "内存");
 //    tools.countFiles("");
 //    tools.printFiles("");
-    tools.count_imgs_and_videos("F:\\未分类\\BLUECAKE Hikari - Vol.04 BAD DOCTOR Succubus (+RED.Ver)  149P", "copy");
-
+//    tools.count_imgs_and_videos("F:\\未分类\\BLUECAKE Hikari - Vol.04 BAD DOCTOR Succubus (+RED.Ver)  149P", "copy");
+    cout << GetFolderSize("E:\\MyCodeProject\\CLionProjects\\CppTools\\CMakeLists.txt") << endl;
     return 0;
 }
 
