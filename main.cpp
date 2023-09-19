29073c5553231d7663a5046038bcb4f4be9d4927
yanzzp <1105770801@qq.com>
2023-09-19 22:09:53 +0800

    update opencv

--- main.cpp
+++ main.cpp
@@ -1,55 +1,18 @@
-//#include "mytools.h"
-//#include "Python.h"
-//
-//using namespace std;
-//
-//
-//int main() {
-////    mytools tools;
-//
-////    tools.create_txt_file("C:\\Users\\Administrator\\Desktop\\test\\test.txt");
-//
-//    Py_Initialize();
-//    if(!Py_IsInitialized()){
-//        cout << "python init fail" << endl;
-//        return 0;
-//    }
-//    // 2、初始化python系统文件路径，保证可以访问到 .py文件
-//    PyRun_SimpleString("import sys");
-//    PyRun_SimpleString("sys.path.append('./script')");
-//
-//    // 3、调用python文件名，不用写后缀
-//    PyObject* pModule = PyImport_ImportModule("sayhello");
-//    if( pModule == NULL ){
-//        cout <<"module not found" << endl;
-//        return 1;
-//    }
-//    // 4、调用函数
-//    PyObject* pFunc = PyObject_GetAttrString(pModule, "say");
-//    if( !pFunc || !PyCallable_Check(pFunc)){
-//        cout <<"not found function add_num" << endl;
-//        return 0;
-//    }
-//    //
-//    PyObject_CallObject(pFunc, NULL);
-//    // 5、结束python接口初始化
-//    Py_Finalize();
-//    return 0;
-//}
+#include "mytools.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
+using namespace std;
 
-
-#include<Python.h>
 
 int main() {
-    Py_Initialize();
+//    mytools tools;
 
-    PyRun_SimpleString("print ('hello')");
+//    tools.create_txt_file("C:\\Users\\Administrator\\Desktop\\test\\test.txt");
 
-    PyRun_SimpleString("import numpy as np");
-
-    Py_Finalize();
-
-    system("pause");
     return 0;
 }
+
+// path=D:/ffmpeg-win64-shared/bin
