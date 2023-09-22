c081ece6ec38e71659a5b7135a729cc2ff421dbc
yanzzp <1105770801@qq.com>
2023-09-22 11:08:40 +0800

    update .gitignore

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/MyCodeProject/CLionProjects/cpp-tools
-# Build directory: E:/MyCodeProject/CLionProjects/cpp-tools/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/jieba/deps")
--- cmake-build-debug/test/CMakeFiles/load_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for load_test.
-# This may be replaced when dependencies are built.
