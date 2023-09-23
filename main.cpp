4e245e348e8ac709ee4e06423054d9c00d728a51
yanzzp <1105770801@qq.com>
2023-09-23 15:32:53 +0800

    update

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 #include "ffmpegTool.h"
 
 
-extern "C" {
-#include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
-#include <libavcodec/avcodec.h>
-}
 
 
 
@@ -15,9 +10,7 @@ int main() {
     mytools mytools;
     ffmpegTool ffmpeg;
 
-//    get_function_running_time([&](){
-//        mytools.multithread_get_folder_size("E:\\分类",true);
-//    });
-    cout << avcodec_configuration() << endl;
+
     return 0;
 }
+
