532711ee751825a4ee55867171f726b48d6bd575
yanzzp <1105770801@qq.com>
2023-10-10 22:52:09 +0800

    update the InputSimulator

---
+++ lib/InputSimulator.h
@@ -0,0 +1,22 @@
+#ifndef CPP_TOOLS_INPUTSIMULATOR_H
+#define CPP_TOOLS_INPUTSIMULATOR_H
+#include <iostream>
+#include <Windows.h>
+#include <utility>
+#include <vector>
+
+class InputSimulator {
+private:
+
+public:
+    void set_position(int x, int y);
+
+    void mouse_single_click();
+
+    void rich_click(int x,int y,int times,bool isSmooth = false);
+
+    void smooth_movement(int OriginX,int OriginY,int TargetX,int TargetY);
+};
+
+
+#endif //CPP_TOOLS_INPUTSIMULATOR_H
--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "lib/multithread.h"
 #include "lib/FFmpegTool.h"
+#include "lib/InputSimulator.h"
+
 
 using namespace std;
 
 int main() {
 
-    FFmpegTool ffmpegTool;
-
+    InputSimulator click;
+    click.rich_click(1500,500,0);
 
     return 0;
 }
