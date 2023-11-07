8e2464bd3e828d0ba0971a04ddd6d81f653d4888
Yanzzp <1105770801@qq.com>
2023-11-07 13:48:19 +0800

    CMakeLists.txt add tencentcloud-sdk-cpp-core support

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <tencentcloud/core/TencentCloud.h>
+#include <tencentcloud/core/Credential.h>
 #include "lib/MyTools.h"
 int main(){
     MyTools myTools;
 
-    myTools.print_all_files("E:\\迅雷下载");
+//    myTools.print_all_files("E:\\迅雷下载");
     return 0;
 }
