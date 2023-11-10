cbe767b71c78f483ca5f9ff48f34d80f07f81098
zpy <1105770801@qq.com>
2023-11-10 15:00:18 +0800

    update install_packets.sh

---
+++ shells/install_packets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 更新包管理器
+sudo apt-get update
+
+# 安装FFmpeg
+sudo apt-get install ffmpeg
+
+# 安装python3的开发库
+sudo apt-get install python3-dev
+
+# 安装FFmpeg的开发库
+sudo apt-get install libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
