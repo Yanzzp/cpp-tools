769cf4e6537026c4c2662a25ddab4fa538dcee65
Yanzzp <1105770801@qq.com>
2023-11-18 21:12:46 +0800

    update CMakeLists.txt to support the mysql

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include  <iostream>
-#include "lib/plugs.h"
+#include <mysql_driver.h>
+#include <mysql_connection.h>
 
 
 using namespace std;
 
 
-int main(){
-    MyTools myTools;
-    myTools.multithread_get_folder_size("D:\\EdgeDownload\\小说\\全部合集");
+int main() {
+    try {
+        // 初始化 MySQL 驱动
+        sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
+
+        // 创建连接
+        std::unique_ptr<sql::Connection> con(driver->connect("tcp://yanzzp.xyz:3306", "YanSQL", "123zpy"));
+
+        // 连接到数据库
+        con->setSchema("ToolsDB");
+
+        // 如果连接成功
+        std::cout << "连接成功" << std::endl;
+    } catch (const sql::SQLException &e) {
+        std::cerr << "连接失败: " << e.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
