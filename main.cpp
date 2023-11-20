8db80b0b6a374ec237d16332babc3060ab2966fe
companyUbuntu <1105770801@qq.com>
2023-11-20 18:55:04 +0800

    update install_packets of MySQL Connector

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ using namespace std;
 
 int main() {
     MyTools tools;
-    tools.print_all_files("E:\\BaiduNetdiskDownload\\娜美妖姬风情万种完具合集150g左右");
+    cout << "Hello, World!" << endl;
     return 0;
 }
--- shells/install_packets.sh
+++ shells/install_packets.sh
@@ -11,3 +11,11 @@ sudo apt-get install python3-dev
 
 # 安装FFmpeg的开发库
 sudo apt-get install libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
+
+# 安装MySQL Connector/C++
+sudo apt-get install libmysqlcppconn-dev
+sudo apt-get install libmysqlclient-dev
+
+# 安装PkgConfig工具
+sudo apt-get install pkg-config
+
