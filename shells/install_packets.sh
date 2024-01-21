8fd365d0f62dbbfdd340fc8422adc32852a70b3f
Yanzzp <zpy362378@gmail.com>
2024-01-21 20:33:35 +0800

    update the install shell

--- shells/install_packets.sh
+++ shells/install_packets.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # 更新包管理器
-sudo apt-get update
+sudo apt update -y
 
 # 安装gcc、g++、cmake、gdb
-sudo apt-get install gcc g++ cmake gdb
+sudo apt install -y gcc g++ cmake gdb
 
 # 安装FFmpeg
-sudo apt-get install ffmpeg
+sudo apt install -y ffmpeg
 
 # 安装python3的开发库
-sudo apt-get install python3-dev
+sudo apt install -y python3-dev
 
 # 安装FFmpeg的开发库
-sudo apt-get install libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
+sudo apt install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev
+
+# 安装redis的开发库
+sudo apt install -y libhiredis-dev
 
 # 安装MySQL Connector/C++
-sudo apt-get install libmysqlcppconn-dev
-sudo apt-get install libmysqlclient-dev
+sudo apt install -y libmysqlcppconn-dev
+sudo apt install -y libmysqlclient-dev
 
 # 安装PkgConfig工具
-sudo apt-get install pkg-config
-
+sudo apt install -y pkg-config
